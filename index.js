fd805e2f22a954419289c675d283713a564eb870
mwendamwenda <benmwenda83@gmail.com>
2022-08-20 14:19:37 +0300

    completeed assignment

---
+++ index.js
@@ -0,0 +1,6 @@
+const divide = (num1=2000, num2=100)=> num1/num2
+    divide(2000,100);
+    const square = (numb1=2)=>numb1**2;
+    square(2);
+    const add =(parameter1 =2,parameter2 = 3)=>parameter1+parameter2;
+    add(2,3);
